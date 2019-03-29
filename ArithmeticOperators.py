793cb1a1562a48930152998509278f8ec84dbdc7
Nipun <30.nipun@gmail.com>
2019-03-29 17:13:12 -0400

    First two questions

---
+++ ArithmeticOperators.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+if ((a >= 1) & (a <= 10000000000)) & ((b >= 1) & (b <= 10000000000)):
+    print(a+b)
+    print(a-b)
+    print(a*b)
---
+++ PythonIf-Else.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+n = int(input())
+
+if (n%2) == 1:
+    print('Weird')
+else:
+    if (n >= 2) & (n <= 5):
+        print("Not Weird")
+    elif (n >= 6) & (n <= 20):
+        print("Weird")
+    elif (n > 20):
+        print("Not Weird")
