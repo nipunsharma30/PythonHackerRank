44e60079abfc449cdfc3fee9fa831f286f6b1571
Nipun <30.nipun@gmail.com>
2019-04-19 14:45:58 -0400

    added another logic for this program

--- FindAString.py
+++ FindAString.py
@@ -1,7 +1,12 @@
 def count_substring(string, sub_string):
+    # count = 0
+    # for i in range(len(string) - len(sub_string) + 1):
+    #     if string[i:i+len(sub_string)] == sub_string:
+    #         count += 1
+
     count = 0
-    for i in range(len(string) - len(sub_string) + 1):
-        if string[i:i+len(sub_string)] == sub_string:
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
             count += 1
     return count
 
